2639c36d4983b3e2502663e7f99e5ddd4a1efc75
Samynuke <samy@nukeoptics.com>
2026-07-23 11:54:08 +0800

    Update q5.py

--- q5.py
+++ q5.py
@@ -1,1 +1,15 @@
+def check_divisibility(num, divisor):
+    """
+    Task 1
+    - Create a function to check if the number (num) is divisible by another number (divisor).
+    - Both num and divisor must be numeric.
+    - Return True if num is divisible by divisor, False otherwise.
+    """
+    if not (isinstance(num, (int, float)) and isinstance(divisor, (int, float))):
+        return -1
 
+    return num % divisor == 0
+
+
+# Task 2
+# Invoke the
